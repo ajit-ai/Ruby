1e67e29812f7db16c881db750aa835c982c9c971
ajitj <ajitk123@gmail.com>
2025-07-24 17:08:45 +0530

    initial commit

---
+++ basic/hello.rb
@@ -0,0 +1,16 @@
+#write a ruby hello world
+puts "Hello World"
+
+#write a ruby hello world with a variable
+greeting = "Hello World Ruby"
+puts greeting
+
+#write a ruby hello world with a variable
+def hello(name)
+    puts "Hello #{name}"
+    end
+hello "Ajit"
+
+puts "Ajit has #{10+5/2} Rupees"
+
+
